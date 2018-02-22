3fb8fb9f6d8bbc075d0f2ac76f9ec76c42479e08
Jonas Bauer <jobauer@mail.uni-mannheim.de>
2018-02-22 16:06:48 +0100

    #7 checkstyle activated

---
+++ src/demoPackage/TestClass.java
@@ -0,0 +1,13 @@
+package demoPackage;
+
+public class TestClass {
+
+  public TestClass() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+
+}
