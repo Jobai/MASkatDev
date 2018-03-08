8cf05f986560c406e22241036b1e707a72f017d2
Jonas Bauer <jobauer@mail.uni-mannheim.de>
2018-03-08 18:07:00 +0100

    #26 Added CommandTypes

---
+++ src/de/skat3/network/datatypes/CommandType.java
@@ -0,0 +1,8 @@
+package de.skat3.network.datatypes;
+
+public enum CommandType {
+    
+    BID_REQUEST, BID_INFO, BID_REDO,
+    PLAY_REQUEST, PLAY_INFO, PLAY_REDO,
+    TRICK_INFO, ROUND_INFO, MATCH_INFO, GAME_INFO;
+}
