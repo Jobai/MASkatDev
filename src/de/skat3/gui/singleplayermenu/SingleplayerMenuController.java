a19439300d541479f52b58230a7fa2019f5c5676
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-03-21 11:10:52 +0100

    #45 Menu Design updated

--- src/de/skat3/gui/singleplayermenu/SingleplayerMenuController.java
+++ src/de/skat3/gui/singleplayermenu/SingleplayerMenuController.java
@@ -1,7 +1,11 @@
 package de.skat3.gui.singleplayermenu;
 
+import javafx.animation.ScaleTransition;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 /**
  * Class to control the corresponding view file.
@@ -10,6 +14,11 @@ import javafx.scene.control.Alert.AlertType;
  */
 public class SingleplayerMenuController {
 
+	private ScaleTransition buttonSizeAnimation;
+
+	/**
+	 * .
+	 */
 	public void startSinglePlayerGame() {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setHeaderText(null);
@@ -18,7 +27,10 @@ public class SingleplayerMenuController {
 
 		alert.showAndWait();
 	}
-	
+
+	/**
+	 * .
+	 */
 	public void startTrainingMode() {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setHeaderText(null);
@@ -27,5 +39,36 @@ public class SingleplayerMenuController {
 
 		alert.showAndWait();
 	}
-	
+
+	/**
+	 * Is starting the animation when the cursor is over a button.
+	 * 
+	 * @param e
+	 *            Event to get the source from.
+	 */
+	public void handleMouseOverEvent(MouseEvent e) {
+		Button source = (Button) e.getSource();
+		buttonSizeAnimation = new ScaleTransition();
+		buttonSizeAnimation.setDuration(Duration.millis(1));
+		buttonSizeAnimation.setNode(source);
+		buttonSizeAnimation.setToX(1.1);
+		buttonSizeAnimation.setToY(1.1);
+		buttonSizeAnimation.play();
+	}
+
+	/**
+	 * Is starting the animation when the cursor is over a button.
+	 * 
+	 * @param e
+	 *            Event to get the source from.
+	 */
+	public void handleMouseLeaveEvent(MouseEvent e) {
+		Button source = (Button) e.getSource();
+		buttonSizeAnimation = new ScaleTransition();
+		buttonSizeAnimation.setDuration(Duration.millis(1));
+		buttonSizeAnimation.setNode(source);
+		buttonSizeAnimation.setToX(1.0);
+		buttonSizeAnimation.setToY(1.0);
+		buttonSizeAnimation.play();
+	}
 }
