9a23e60df8649676d32cc2b76f2b1c49fde0e0cf
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-04-03 00:40:33 +0200

    #45 little menu update

--- src/de/skat3/gui/multiplayermenu/MultiplayerMenuController.java
+++ src/de/skat3/gui/multiplayermenu/MultiplayerMenuController.java
@@ -1,10 +1,7 @@
 package de.skat3.gui.multiplayermenu;
 
-import com.sun.medialib.mlib.Image;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 
 /**
  * Class to control the corresponding view file.
@@ -13,11 +10,11 @@ import javafx.scene.image.ImageView;
  */
 public class MultiplayerMenuController {
 
-	@FXML
-	public Button refreshButton;
+  @FXML
+  public Button refreshButton;
 
-	public MultiplayerMenuController() {
-		//refreshButton = new Button("", new ImageView("../../../../../../../Downloads/test.png"));
-	}
-	
+  public MultiplayerMenuController() {
+
+  }
+
 }
--- src/de/skat3/gui/singleplayermenu/SingleplayerMenuController.java
+++ src/de/skat3/gui/singleplayermenu/SingleplayerMenuController.java
@@ -3,6 +3,7 @@ package de.skat3.gui.singleplayermenu;
 import javafx.animation.ScaleTransition;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
@@ -14,61 +15,59 @@ import javafx.util.Duration;
  */
 public class SingleplayerMenuController {
 
-	private ScaleTransition buttonSizeAnimation;
+  private ScaleTransition imageSizeAnimation;
 
-	/**
-	 * .
-	 */
-	public void startSinglePlayerGame() {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setHeaderText(null);
-		alert.setTitle("Information Dialog");
-		alert.setContentText("SinglePlayer Game started!");
+  /**
+   * .
+   */
+  public void startSinglePlayerGame() {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setHeaderText(null);
+    alert.setTitle("Information Dialog");
+    alert.setContentText("SinglePlayer Game started!");
 
-		alert.showAndWait();
-	}
+    alert.showAndWait();
+  }
 
-	/**
-	 * .
-	 */
-	public void startTrainingMode() {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setHeaderText(null);
-		alert.setTitle("Information Dialog");
-		alert.setContentText("TrainingMode started!");
+  /**
+   * .
+   */
+  public void startTrainingMode() {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setHeaderText(null);
+    alert.setTitle("Information Dialog");
+    alert.setContentText("TrainingMode started!");
 
-		alert.showAndWait();
-	}
+    alert.showAndWait();
+  }
 
-	/**
-	 * Is starting the animation when the cursor is over a button.
-	 * 
-	 * @param e
-	 *            Event to get the source from.
-	 */
-	public void handleMouseOverEvent(MouseEvent e) {
-		Button source = (Button) e.getSource();
-		buttonSizeAnimation = new ScaleTransition();
-		buttonSizeAnimation.setDuration(Duration.millis(1));
-		buttonSizeAnimation.setNode(source);
-		buttonSizeAnimation.setToX(1.1);
-		buttonSizeAnimation.setToY(1.1);
-		buttonSizeAnimation.play();
-	}
+  /**
+   * Is starting the animation when the cursor is over a button.
+   * 
+   * @param e Event to get the source from.
+   */
+  public void handleMouseOverEvent(MouseEvent e) {
+    ImageView source = (ImageView) e.getSource();
+    imageSizeAnimation = new ScaleTransition();
+    imageSizeAnimation.setDuration(Duration.millis(1));
+    imageSizeAnimation.setNode(source);
+    imageSizeAnimation.setToX(1.1);
+    imageSizeAnimation.setToY(1.1);
+    imageSizeAnimation.play();
+  }
 
-	/**
-	 * Is starting the animation when the cursor is over a button.
-	 * 
-	 * @param e
-	 *            Event to get the source from.
-	 */
-	public void handleMouseLeaveEvent(MouseEvent e) {
-		Button source = (Button) e.getSource();
-		buttonSizeAnimation = new ScaleTransition();
-		buttonSizeAnimation.setDuration(Duration.millis(1));
-		buttonSizeAnimation.setNode(source);
-		buttonSizeAnimation.setToX(1.0);
-		buttonSizeAnimation.setToY(1.0);
-		buttonSizeAnimation.play();
-	}
+  /**
+   * Is starting the animation when the cursor is over a button.
+   * 
+   * @param e Event to get the source from.
+   */
+  public void handleMouseLeaveEvent(MouseEvent e) {
+    ImageView source = (ImageView) e.getSource();
+    imageSizeAnimation = new ScaleTransition();
+    imageSizeAnimation.setDuration(Duration.millis(1));
+    imageSizeAnimation.setNode(source);
+    imageSizeAnimation.setToX(1.0);
+    imageSizeAnimation.setToY(1.0);
+    imageSizeAnimation.play();
+  }
 }
