598164d691156dcef6ad67faa32576cab671e8a3
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-04-18 00:35:47 +0200

    #31 result screens added

---
+++ src/de/skat3/gui/resultscreen/GameResultViewController.java
@@ -0,0 +1,27 @@
+package de.skat3.gui.resultscreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameResultViewController {
+
+  @FXML
+  private Label nameP4;
+  @FXML
+  private Label nameP3;
+  @FXML
+  private Label nameP2;
+  @FXML
+  private Label winner;
+  @FXML
+  private Label nameP1;
+  @FXML
+  private Label pointsP1;
+  @FXML
+  private Label pointsP2;
+  @FXML
+  private Label pointsP3;
+  @FXML
+  private Label pointsP4;
+
+}
