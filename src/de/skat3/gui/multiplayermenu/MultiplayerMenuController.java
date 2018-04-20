2943fe7e70a0bd325fc2c7a5878392301359da30
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-04-21 01:15:46 +0200

    #31 Multiplayer Server Lobby

--- src/de/skat3/gui/multiplayermenu/MultiplayerMenuController.java
+++ src/de/skat3/gui/multiplayermenu/MultiplayerMenuController.java
@@ -1,28 +1,31 @@
 package de.skat3.gui.multiplayermenu;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Optional;
+import de.skat3.gamelogic.Player;
 import de.skat3.main.Lobby;
 import de.skat3.main.SkatMain;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -38,19 +41,73 @@ public class MultiplayerMenuController {
   @FXML
   private Label serverPlayer;
   @FXML
-  private Button refreshButton;
+  private ImageView imageP2;
+  @FXML
+  private ImageView imageP1;
   @FXML
   private Label serverName;
+  @FXML
+  private ImageView imageP4;
+  @FXML
+  private ImageView imageP3;
+  @FXML
+  private ListView<String> hostListView;
+  @FXML
+  private Label nameP4;
+  @FXML
+  private Label nameP3;
+  @FXML
+  private Label nameP2;
+  @FXML
+  private Label nameP1;
+  @FXML
+  private Button refreshButton;
   @FXML
   private Label serverIP;
   @FXML
   private Label serverPW;
-  @FXML
-  private ListView<String> hostListView;
-  
+
   Lobby currentLobby;
 
   ArrayList<Lobby> hostList = new ArrayList<Lobby>();
+
+
+  public static class HostCell extends ListCell<String> {
+    HBox hbox = new HBox();
+    Image serverImage = new Image("guifiles/AppIcon.png");
+    Image passwordImage = new Image("guifiles/lock.png");
+    Image timerImage = new Image("guifiles/alarm-clock.png");
+    ImageView ivServer = new ImageView(serverImage);
+    ImageView ivPassword = new ImageView(passwordImage);
+    ImageView ivTimer = new ImageView(timerImage);
+    Pane pane = new Pane();
+    Label label = new Label();
+
+    public HostCell() {
+      super();
+      ivServer.setFitWidth(30);
+      ivServer.setFitHeight(30);
+      hbox.setPrefHeight(50);
+      hbox.setAlignment(Pos.CENTER_LEFT);
+      hbox.setSpacing(20);
+      label.setFont(new Font(20));
+      hbox.getChildren().addAll(ivServer, label, ivPassword, ivTimer, pane);
+    }
+
+    public void updateItem(String name, boolean empty) {
+      super.updateItem(name, empty);
+      setText(null);
+      setGraphic(null);
+
+      if (name != null && !empty) {
+        label.setText(name);
+        setGraphic(hbox);
+      }
+    }
+
+
+  }
+
 
   /**
    * .
@@ -62,23 +119,27 @@ public class MultiplayerMenuController {
   public void startGame() {
     SkatMain.mainController.startGame();
   }
-  
+
   /**
    * .
    */
   public void fillHostList() {
-    ObservableList<String> items = FXCollections.observableArrayList();
+
+    hostListView.setCellFactory(param -> new HostCell());
 
     new Thread() {
       public void run() {
+        ObservableList<String> items = FXCollections.observableArrayList("Server 1 | Players (1/3)",
+            "Serer 2 | Players (2/4)", "Server 3 | Players (2/4)", "Server 4 | Players (3/4)");
+
         hostList = SkatMain.mainController.getLocalHosts();
-        System.out.println("ICh bin ");
         for (Lobby lobby : hostList) {
           items.add(lobby.getName() + "(" + lobby.getCurrentNumberOfPlayers() + "/"
               + lobby.getMaximumNumberOfPlayers() + ")");
         }
 
         hostListView.setItems(items);
+
       };
     }.start();
 
@@ -147,9 +208,34 @@ public class MultiplayerMenuController {
     // fill view fields
     serverName.setText(currentLobby.getName());
     serverIP.setText(currentLobby.getIp().toString());
-    // serverPW.setText(currentLobby);
+    // serverPW.setText(currentLobby.);
     serverPlayer.setText(
         currentLobby.getCurrentNumberOfPlayers() + "/" + currentLobby.getMaximumNumberOfPlayers());
+
+    // Players
+    Player[] players = currentLobby.getPlayers();
+
+    for (int i = 0; i < players.length; i++) {
+      if (i == 0) {
+        // nameP1.setText(players[i]);
+        // imageP1.setImage(players[i]);
+      } ;
+
+      if (i == 1) {
+        // nameP2.setText(players[i]);
+        // imageP2.setImage(players[i]);
+      } ;
+
+      if (i == 2) {
+        // nameP3.setText(players[i]);
+        // imageP3.setImage(players[i]);
+      } ;
+
+      if (i == 3) {
+        // nameP4.setText(players[i]);
+        // imageP4.setImage(players[i]);
+      } ;
+    }
 
   }
 
@@ -167,13 +253,13 @@ public class MultiplayerMenuController {
         ImageView v = new ImageView(i1);
         g.getChildren().add(v);
         screen.setScene(scene);
-        screen.setWidth(400);
-        screen.setHeight(400);
+        screen.setWidth(131);
+        screen.setHeight(131);
         screen.setAlwaysOnTop(true);
         screen.initModality(Modality.APPLICATION_MODAL);
         screen.initStyle(StageStyle.UNDECORATED);
 
-        PauseTransition delay = new PauseTransition(Duration.seconds(5));
+        PauseTransition delay = new PauseTransition(Duration.seconds(10));
         delay.setOnFinished(event -> screen.close());
         delay.play();
 
