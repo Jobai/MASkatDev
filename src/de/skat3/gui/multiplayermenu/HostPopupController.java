bcc19d9331895c429981f1390a0cea85b2357537
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-04-24 15:49:00 +0200

    #31 Kontra/Rekontra added + Bugfix

--- src/de/skat3/gui/multiplayermenu/HostPopupController.java
+++ src/de/skat3/gui/multiplayermenu/HostPopupController.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -28,6 +29,8 @@ public class HostPopupController {
   private ChoiceBox<String> mode;
   @FXML
   private TextField modeValue;
+  @FXML
+  private CheckBox kontraRekontra;
 
   @FXML
   private void initialize() {
@@ -38,47 +41,42 @@ public class HostPopupController {
     mode.setItems(modeList);
   }
 
-  public HostPopupController() {
-
-  }
-
   public void setStage(Stage s) {
     this.hostPopup = s;
   }
 
+  /**
+   * .
+   * 
+   * @throws NumberFormatException TODO
+   * @throws UnknownHostException TODO
+   */
   public void hostGame() throws NumberFormatException, UnknownHostException {
-
-    // public void hostMultiplayerGame(String name, int numberOfPlayers, int timer,
-    // boolean kontraRekontraEnabled, int scoringMode) throws UnknownHostException {
-    // this.hostMultiplayerGame(name, "", numberOfPlayers, timer, kontraRekontraEnabled,
-    // scoringMode);
-    //
-    // }
-    //
-    // public void hostMultiplayerGame(String name, String password, int numberOfPlayers, int timer,
-    // boolean kontraRekontraEnabled, int scoringMode) throws UnknownHostException {
-
-
 
     int players = playerCount.getValue();
     int intTimer;
-    if (timer.getText() == "") {
+    if (timer.getText().isEmpty()) {
       intTimer = 0;
     } else {
       intTimer = Integer.parseInt(timer.getText());
     }
 
-    if (password.getText() == "") {
-      SkatMain.mainController.hostMultiplayerGame(serverName.getText(), players, intTimer, false,
-          Integer.parseInt(modeValue.getText()));
+    if (password.getText().isEmpty()) {
+      SkatMain.mainController.hostMultiplayerGame(serverName.getText(), players, intTimer,
+          kontraRekontra.isSelected(), Integer.parseInt(modeValue.getText()));
     } else {
       SkatMain.mainController.hostMultiplayerGame(serverName.getText(), password.getText(), players,
-          intTimer, false, Integer.parseInt(modeValue.getText()));
+          intTimer, kontraRekontra.isSelected(), Integer.parseInt(modeValue.getText()));
     }
 
     hostPopup.close();
   }
 
+  /**
+   * . TODO
+   * 
+   * @param e Event
+   */
   public void switchMode(ActionEvent e) {
     if (mode.getValue() == "Seeger") {
       modeValue.setText(String.valueOf(48));
