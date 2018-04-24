6409974e6f69e6552611332db7ca0e285f78c4e9
Artem Zamarajev <azamaraj@mail.uni-mannheim.de>
2018-04-25 01:06:44 +0200

    #57 added Statistics, #9 made refactoristion, #55 added Statistics tests

--- src/de/skat3/io/profile/IoController.java
+++ src/de/skat3/io/profile/IoController.java
@@ -2,28 +2,53 @@ package de.skat3.io.profile;
 
 import static de.skat3.io.profile.Utils.GSON;
 import static de.skat3.io.profile.Utils.JSON_PATH_PRODUCTION;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.UUID;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.scene.image.Image;
 
 
 public class IoController implements IoInterface {
-  public JSONProfileReader reader;
+
   private ArrayList<Profile> profileList;
+  private Profile lastUsed;
 
   public IoController() {
-    this.reader = new JSONProfileReader();
-    this.profileList = getProfileList();
+    this.profileList = getAllProfileAsList();
+    determineLastUsedProfile();
+  }
+
+  @Override
+  public Profile getLastUsedProfile() {
+    determineLastUsedProfile();
+    return lastUsed;
+  }
+
+  public void setLastUsedProfile(Profile lastUsed) {
+    this.lastUsed = lastUsed;
+  }
+
+  @Override
+  public ArrayList<Profile> getProfileList() {
+    return this.profileList;
+  }
+
+  public void setProfileList(ArrayList<Profile> profileList) {
+    this.profileList = profileList;
   }
 
   @Override
   public void addProfile(Profile profile) {
     profileList.add(profile);
-    updateProfiles();
-    profile.setLastUsedTrue(this);
+    updateLastUsed(profile);
   }
 
   @Override
@@ -34,6 +59,12 @@ public class IoController implements IoInterface {
     return deleted;
   }
 
+
+  /**
+   * toEdit is needed to produce a NullPointerException if profile is not in the profileList That
+   * should not happen, as the profile should not be showed in GUI thats why exception is not being
+   * catched
+   */
   @Override
   public void editProfile(Profile profile, String name, Image image, String imageFormat) {
     Profile toEdit = null;
@@ -57,28 +88,40 @@ public class IoController implements IoInterface {
 
   @Override
   public Profile readProfile(UUID id) {
-    Profile profile = reader.readProfile(id);
-    profile.setLastUsedTrue(this);
-
-    return profile;
+    Iterator<Profile> iter = profileList.iterator();
+    while (iter.hasNext()) {
+      Profile current = iter.next();
+      if (current.getUuid().equals(id)) {
+        updateLastUsed(current);
+        return current;
+      }
+    }
+    return null;
   }
 
-  @Override
-  public ArrayList<Profile> getProfileList() {
-    return reader.getProfileList();
+  /**
+   * @param profile is a profile to be set as current
+   * 
+   *        Calls at @param profile setLastUsed(true), and setLastUsed(false) for all other profiles
+   *        then calls updateProfiles()
+   */
+  public void updateLastUsed(Profile profile) {
+    profile.setLastUsed(true);
+    Iterator<Profile> iter = profileList.iterator();
+    while (iter.hasNext()) {
+      Profile current = iter.next();
+      if (!current.equals(profile)) {
+        current.setLastUsed(false);
+      }
+    }
+    updateProfiles();
   }
 
-  @Override
-  public Profile getLastUsedProfile() {
-    return reader.getLastUsedProfile();
-  }
-
-  public JSONProfileReader getReader() {
-    return reader;
-  }
-
-  public void updateProfiles() {
-    reader.determineLastUsedProfile();
+  /**
+   * calls determine determineLastUsedProfile() and writes profile list to json file
+   */
+  private void updateProfiles() {
+    determineLastUsedProfile();
     String profileJsonString = GSON.toJson(profileList);
     try {
       FileWriter writer = new FileWriter(JSON_PATH_PRODUCTION);
@@ -87,6 +130,66 @@ public class IoController implements IoInterface {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Determines the last used profile Used to initialize and update lastUsed Is called by
+   * updateProfiles()
+   */
+  private void determineLastUsedProfile() {
+    lastUsed = null;
+    if (profileList.size() <= 0) {
+      return;
+    } else {
+      Iterator<Profile> itr = profileList.iterator();
+      while (itr.hasNext()) {
+        Profile potentiallyCurrent = itr.next();
+        if (potentiallyCurrent.getLastUsed()) {
+          lastUsed = potentiallyCurrent;
+        }
+      }
+      if (lastUsed == null) {
+        lastUsed = profileList.get(0);
+      }
+    }
+  }
+
+  /**
+   * Reads all profiles from json file Used only to initialize profileList
+   * 
+   * @return list of all profiles
+   */
+  private ArrayList<Profile> getAllProfileAsList() {
+    ArrayList<Profile> list = new ArrayList<Profile>();
+    JsonArray jsonArray = convertFileToJsonArray();
+    for (int i = 0; i < jsonArray.size(); i++) {
+      JsonObject profileElement = jsonArray.get(i).getAsJsonObject();
+      Profile profileObject = GSON.fromJson(profileElement, Profile.class);
+      profileObject.setImageFromEncodedString();
+      list.add(profileObject);
+    }
+    return list;
+  }
+
+  /**
+   * Converts file to JsonArray Used only in getAllProfilesAsList()
+   * 
+   * 
+   * @return JsonArray consisting of all profiles
+   */
+  private JsonArray convertFileToJsonArray() {
+    JsonArray jsonArray = new JsonArray();
+    try {
+      JsonParser parser = new JsonParser();
+      JsonElement jsonElement = parser.parse(new FileReader(JSON_PATH_PRODUCTION));
+      jsonArray = jsonElement.getAsJsonArray();
+    } catch (FileNotFoundException e) {
+      System.out.println("The json file was not found");
+      e.printStackTrace();
+    } catch (NullPointerException ex) {
+      return null;
+    }
+    return jsonArray;
   }
 }
 
--- test/io/IoTest.java
+++ test/io/IoTest.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 // FIXME Tests should be ran with empty profiles.json and will leave the file empty as well
 @RunWith(Suite.class)
-@SuiteClasses({testImageConvertion.class, testLastUsedProfile.class, testProfileManagement.class})
+@SuiteClasses({TestImageConvertion.class, TestLastUsedProfile.class, TestProfileManagement.class,
+    TestProfileStatistics.class})
 public class IoTest {
 
 }
