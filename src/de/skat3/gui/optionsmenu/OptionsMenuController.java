f8f3e239379ba4c5f0d9a1111620fd2280e83262
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-04-24 23:05:02 +0200

    #31 Options Menu

--- src/de/skat3/gui/optionsmenu/OptionsMenuController.java
+++ src/de/skat3/gui/optionsmenu/OptionsMenuController.java
@@ -1,8 +1,14 @@
 package de.skat3.gui.optionsmenu;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
 
 /**
  * Class to control the corresponding view file.
@@ -18,6 +24,44 @@ public class OptionsMenuController {
   private Slider volumeMusic;
   @FXML
   private ToggleButton btnMusic;
-  
+
+
+  public void handleMusicSwitched() {
+
+    if (btnMusic.isSelected()) {
+      // Music off
+
+    } else {
+      // Music on
+      System.out.println("ON");
+      
+      AudioPlayer MGP = AudioPlayer.player;
+      AudioStream BGM;
+      AudioData MD;
+      ContinuousAudioDataStream loop = null;
+      try {
+        BGM = new AudioStream(new FileInputStream("../../../../backgroundMusic.mp3"));
+        MD = BGM.getData();
+        loop = new ContinuousAudioDataStream(MD);
+      } catch (IOException error) {
+        System.out.print("file not found");
+      }
+      MGP.start(loop);
+
+    }
+
+  }
+
+  public void handleGameSoundSwitched() {
+
+  }
+
+  public void handleVolumeMusicChanged() {
+
+  }
+
+  public void handleVolumeGameSoundChanged() {
+
+  }
 
 }
