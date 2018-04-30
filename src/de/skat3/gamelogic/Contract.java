Jobai/MASkatDev

commit 023332f181a40047aa1ed09737e62f31922ef32d

#32 #28 implemented suit unicode symbols + fixes pw check

src/de/skat3/gamelogic/Contract.java
@@ -5,4 +5,25 @@ import java.io.Serializable;
 public enum Contract implements Serializable {
 
   DIAMONDS, HEARTS, SPADES, CLUBS, GRAND, NULL;
+
+
+  @Override //JB
+  public String toString() {
+    switch (this) {
+      case DIAMONDS:
+        return "DIAMONDS ♦";
+      case HEARTS:
+        return "HEARTS ♥";
+      case SPADES:
+        return "SPADES ♠";
+      case CLUBS:
+        return "CLUBS ♣";
+      case GRAND:
+        return "GRAND";
+      case NULL:
+        return "NULL";
+      default:
+        throw new AssertionError();
+    }
+  }
 }

src/de/skat3/gui/multiplayermenu/MultiplayerMenuController.java
@@ -204,7 +204,7 @@ public class MultiplayerMenuController {
     if (!currentLobby.isHasPassword()) {
       SkatMain.mainController.joinMultiplayerGame(currentLobby);
       System.out.println("Join");
-    } else {
+    } else { //implemented password input - JB 29.04.2018
       TextInputDialog dialog = new TextInputDialog("");
       dialog.setTitle("Enter lobby password");
       dialog.setHeaderText("Enter lobby password");

src/de/skat3/main/Lobby.java
@@ -44,7 +44,7 @@ public class Lobby implements Serializable {
   boolean singlePlayerGame;
   boolean kontraRekontraEnabled;
   private UUID uuid;
-  
+
   boolean hasPassword;
 
   /**
@@ -67,8 +67,13 @@ public class Lobby implements Serializable {
     this.serverMode = serverMode;
     this.currentPlayers = 0;
     this.name = name;
-    this.password = password;
-    this.hasPassword = true;
+
+    if (password != null && !password.isEmpty()) {
+      this.password = password;
+      this.hasPassword = true;
+    } else {
+      this.hasPassword = false;
+    }
     this.scoringMode = scoringMode;
     this.kontraRekontraEnabled = kontraRekontraEnabled;
     uuid = UUID.randomUUID();

commit 76db3ecb08fa4149ba2befd7b4d4e067bcb83270

#32 fixed contract selection bug
introduced by 023332f181a40047aa1ed09737e62f31922ef32d

src/de/skat3/gamelogic/Contract.java
@@ -7,7 +7,38 @@ public enum Contract implements Serializable {
   DIAMONDS, HEARTS, SPADES, CLUBS, GRAND, NULL;
 
 
-  @Override //JB
+
+  /**
+   * Converts a String to the corresponding enum.
+   * @author Jonas Bauer
+   * @param s String to be converted
+   * @return corresponding Contract enum.
+   */
+  public static Contract getEnum(String s) {
+    switch (s) {
+      case "DIAMONDS":
+      case "DIAMONDS ♦":
+        return Contract.DIAMONDS;
+      case "HEARTS":
+      case "HEARTS ♥":
+        return Contract.HEARTS;
+      case "SPADES":
+      case "SPADES ♠":
+        return Contract.SPADES;
+      case "CLUBS":
+      case "CLUBS ♣":
+        return Contract.CLUBS;
+      case "GRAND":
+        return Contract.GRAND;
+      case "NULL":
+        return Contract.NULL;
+      default:
+        throw new AssertionError();
+    }
+
+  }
+
+  @Override // JB
   public String toString() {
     switch (this) {
       case DIAMONDS:

src/de/skat3/gui/GuiController.java
@@ -75,7 +75,7 @@ public class GuiController implements GuiControllerInterface {
 
     Optional<String> result = dialog.showAndWait();
     if (result.isPresent()) {
-      SkatMain.mainController.contractSelected(Contract.valueOf(result.get()),
+      SkatMain.mainController.contractSelected(Contract.getEnum(result.get()),
           new AdditionalMultipliers());
     }
 