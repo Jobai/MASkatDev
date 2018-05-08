8ceefe22c6668e32d206a2ed02ab24dcc2bb8b42
Artem Zamarajev <azamaraj@mail.uni-mannheim.de>
2018-05-08 11:32:19 +0200

    #32 fixed gui imports

--- src/de/skat3/main/SkatMain.java
+++ src/de/skat3/main/SkatMain.java
@@ -1,16 +1,11 @@
 
-
 package de.skat3.main;
 
-import java.util.Properties;
 import de.skat3.gui.Gui;
 import de.skat3.gui.GuiController;
 import de.skat3.io.SoundPlayer;
 import de.skat3.io.SoundVolumeUtil;
 import de.skat3.io.profile.IoController;
-import de.skat3.main.AiController;
-import de.skat3.main.LocalGameState;
-import de.skat3.main.MainController;
 import de.skat3.network.MainNetworkController;
 import de.skat3.network.client.ClientLogicController;
 
@@ -36,17 +31,15 @@ public class SkatMain {
   public static void main(String[] args) {
     Thread.currentThread().setName("MainThread");
     SkatMain.masterLogger = new MasterLogger();
-    SkatMain.soundPlayer = new SoundPlayer();
-    soundPlayer.playBackgroundMusic();
-    SkatMain.soundVolumeUtil = new SoundVolumeUtil();
+    // SkatMain.soundPlayer =
+    // (new SoundPlayer()).start();
+    // SkatMain.soundVolumeUtil = new SoundVolumeUtil();
     SkatMain.mainController = new MainController();
     SkatMain.guiController = new GuiController();
     SkatMain.ioController = new IoController();
     SkatMain.mainNetworkController = new MainNetworkController();
     SkatMain.aiController = new AiController();
     Gui.showAndWait();
-
-
 
   }
 }
