829e3fdae34aebda1c74b7e52a87360ab20ffb37
Timo Straub <tistraub@mail.uni-mannheim.de>
2018-05-09 19:40:06 +0200

    #31 round cancelled button fix

--- src/de/skat3/gui/matchfield/InGameOverlayController.java
+++ src/de/skat3/gui/matchfield/InGameOverlayController.java
@@ -368,7 +368,7 @@ public class InGameOverlayController {
         p = loader.load();
       } catch (IOException e) {
       }
-      Button closeButton = (Button) p.lookup("closeButton");
+      Button closeButton = (Button) p.getChildren().get(1);
 
       this.addAndSetupButton(p, closeButton);
       return;
